546eda581c96773c6f0903c26b6c30adb2fc9683
Alberto Anguita <alberto.anguita@gmail.com>
2016-06-19 13:36:02 +0200

    Add movie controller and view (empty)

---
+++ src/main/java/jacz/face/controllers/MovieController.java
@@ -0,0 +1,15 @@
+package jacz.face.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Alberto on 19/06/2016.
+ */
+public class MovieController extends GenericController {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // todo
+    }
+}
